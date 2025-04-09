9f313282f5769be0ee4e4ab1fcb59b61f49c1856
Jin Hyun Jamie Park <jinhyunpark929@gmail.com>
2025-04-09 10:50:26 +0900

    [Update] junit fail test

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -2,12 +2,20 @@ package edu.fisa.ce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals; // JUnit 5
+
 
 @SpringBootTest
 class Step07CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int result = 12 + 3;
+
+		System.out.println("비교 전*************");
+		assertEquals(1, result); // 성공: 5 == 5
+
+		System.out.println("비교 후*************");
 	}
 
 }
